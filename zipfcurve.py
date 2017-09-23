7de97363d9c59afadcbd09d47fa47369c9694ae2
rodvieirasilva <rodvieirasilva@gmail.com>
2017-09-23 18:06:21 -0300

    adicionado uniques

--- zipfcurve.py
+++ zipfcurve.py
@@ -12,6 +12,7 @@ class ZipfCurve:
         for word in self.words:
             count = self.frequency.get(word,0)
             self.frequency[word] = count + 1
+        self.sorted_frequency = sorted(self.frequency.items(), key=lambda x:x[1], reverse=True)
 
     def plot(self):
         i=0
@@ -20,21 +21,28 @@ class ZipfCurve:
         values = [[i,item] for i, item in enumerate(values)]
         plt.plot([row[0] for row in values], [row[1] for row in values])
         plt.show()
-        
-    def lunh(self, percent):
-        sorted_frequency = sorted(self.frequency.items(), key=lambda x:x[1], reverse=True)
-        size = len(sorted_frequency)
+
+    def lunh(self, percent):        
+        size = len(self.sorted_frequency)
         sizePrint = int(size * (percent / 100.0)) 
         for i in range(0, sizePrint):            
-            print(sorted_frequency[i])
-            print(sorted_frequency[size - i - 1])
+            print(self.sorted_frequency[i])
+            print(self.sorted_frequency[size - i - 1])
+    
+    def uniques(self):              
+        result = [value for key,value in self.frequency.items() if value == 1]
+        print(result)
+        print('Size: {}'.format(len(result)))
+        return result
+            
 
 def main():
     with open('allwords.json') as json_data:
         allwords = json.load(json_data)
         zipf = ZipfCurve(allwords)
         zipf.lunh(0.1)
-        zipf.plot()        
+        zipf.plot()   
+        zipf.uniques()     
     print("Finish")
 
 if __name__ == '__main__':
