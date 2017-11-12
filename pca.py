9e47fc3cf6bdff519286f5bd06875d37f7206819
rodvieirasilva <rodvieirasilva@gmail.com>
2017-11-12 16:40:38 -0200

    adicionado legenda pcae  geração de cores

--- pca.py
+++ pca.py
@@ -6,60 +6,9 @@ import numpy as np
 import re
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-
+import colorsys
 
 class PlotPCA:
-    #50 cores distintas 
-    basec = ["#2d9590",
-            "#da4c23",
-            "#4f62d0",
-            "#68c243",
-            "#8d47b7",
-            "#54c76a",
-            "#9c6fe8",
-            "#abbc35",
-            "#db74dc",
-            "#4b9b30",
-            "#ab3a9b",
-            "#53cc91",
-            "#e6437d",
-            "#369a5b",
-            "#d8519e",
-            "#3f7821",
-            "#8f85e1",
-            "#e4a73b",
-            "#4481d2",
-            "#e17c2e",
-            "#4cbee0",
-            "#d04133",
-            "#5dcebc",
-            "#db3750",
-            "#48a888",
-            "#ae3364",
-            "#3e7b39",
-            "#a775bb",
-            "#7a9533",
-            "#605a9c",
-            "#beaa40",
-            "#6798d1",
-            "#ae7b22",
-            "#da93cd",
-            "#6ead72",
-            "#b03939",
-            "#2c744d",
-            "#e9735b",
-            "#a9c277",
-            "#984f77",
-            "#8f9353",
-            "#de6c77",
-            "#676722",
-            "#e5869c",
-            "#caa569",
-            "#a14d55",
-            "#e3946e",
-            "#aa4e1e",
-            "#946332",
-            "#ac5b49"]
     
     def __init__(self, filename=None, data=None):
         if(filename != None):
@@ -70,13 +19,20 @@ class PlotPCA:
         self.pca = PCA(n_components=2, svd_solver='full')
         self.pca.fit(self.bagofwords)
 
+    def gencolors(self, n):
+        return [colorsys.hsv_to_rgb(x*1.0/n, 0.5, 0.7) for x in range(n)]
+
     def plotpca(self, title, words, y_pred, classnames):
         plt.figure(num=title)
         plt.title(title)
         XPCA = self.pca.transform(words)
-        colors = [self.basec[ (c % len(self.basec)) ] for c in y_pred]
-        patches = [mpatches.Patch(color=self.basec[ (i % len(self.basec)) ], label=classn) for i,classn in enumerate(classnames)]
+        basec = self.gencolors(len(classnames))
+        colors = [basec[ (c % len(basec)) ] for c in y_pred]
         plt.scatter(XPCA[:, 0], XPCA[:, 1], s=30, color=colors)
+        if len(classnames) <= 6:
+            patches = [mpatches.Patch(color=basec[ (i % len(basec)) ], label=classn) for i,classn in enumerate(classnames)]
+            plt.legend(handles=patches)
+            plt.legend()
 
     def show(self):
         plt.show()
@@ -101,9 +57,11 @@ def main():
     dictthemeclass = {0:{0:0,1:1},1:{0:2, 1:3}, 2:{0:4, 1:5}}
     Yclasstheme = [dictthemeclass[item[1]][item[0]] for item in Ys]    
     pca.plotpca("pca_class", pca.bagofwords, Yclazz , ["negative", "positive"])
+    pca.savefig('pca/pca_class.png')
     pca.plotpca("pca_theme", pca.bagofwords, Ytheme, ["game", "movie", "smartphone"])
+    pca.savefig('pca/pca_theme.png')
     pca.plotpca("pca_classtheme", pca.bagofwords, Yclasstheme , ["negative_game","positive_game","negative_movie","positive_movie", "negative_smartphone", "positive_smartphone"])
-    pca.show()
+    pca.savefig('pca/pca_classtheme.png')
     print("Finish")
 
 if __name__ == '__main__':
